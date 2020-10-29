ad1e9f988b5082478eb35410c6253c43e9532d91
Stefan Köhnen <s.koehnen@fz-juelich.de>
2020-10-29 11:36:49 +0100

    Testing

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from jsonschema import validate
+import json
+from jsonschema import validate, Draft7Validator
+
+def get_property_list(schema_dictionary):
+    print(schema_dictionary["properties"])
 
 # Read schema
-
+filename = "openMINDS/v3.0/core/dataset.schema.json"    # Testing with dataset schema
+with open(filename,'r') as f:
+    schema_dictionary = json.loads(f.read())
 
 # Verify schema
+    Draft7Validator.check_schema(schema_dictionary)
 
 # Convert schema to class
+    #print(schema_dictionary)
 
-cls = type('A', (object,), {'__doc__': 'class created by type'})
+    #test = get_property_list(schema_dictionary)
 
-print(cls)
-print(cls.__doc__)
+    class_dictionary = {"__doc__": schema_dictionary["description"]}
+
+    for property in schema_dictionary["properties"]:
+        #print(property)
+        class_dictionary[property] = None
+
+    #print(class_dictionary)
+    cls = type("Dataset", (object,), class_dictionary)
+
+    print(cls)
+    print(cls.__doc__)
+    print(vars(cls))
