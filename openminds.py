9364eff92a89059ed15c5f50cc5ad80671d1fde8
Stefan Köhnen <s.koehnen@fz-juelich.de>
2020-10-30 10:39:57 +0100

    Add constructor to openMINDS

--- openminds.py
+++ openminds.py
@@ -1,4 +1,13 @@
+import os.path
+from pathlib import Path
+
 class OpenMINDS:
     def __init__(self):
         for filename in Path('./openMINDS').glob('**/*.schema.json'):
-            print(filename)
+            stripped_filename = os.path.splitext(
+                                    os.path.splitext(
+                                        os.path.basename(filename)
+                                    )[0]
+                                )[0]
+
+            setattr(self, stripped_filename.upper(), stripped_filename)
